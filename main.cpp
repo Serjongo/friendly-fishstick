0dff5a8daa7e8e0e2340da6be0efe429a0416f2e
Serjongo <Serjongo@gmail.com>
2024-09-15 17:58:37 +0300

    started project, delcared constants and registers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
+#include <cmath> //may not need it, used originally for pow
+
+//shortcuts for code readability
+using BYTE = unsigned char; //8-bit number
+using WORD = unsigned short; //16-bit number, 2byte
+using DWORD = unsigned int; // 32-bit number
+
+//declarations - data structures etc
+WORD memory[65536]; //2^16 bytes
+
+BYTE AF_reg[2]; //Accumulator + Flags
+BYTE BC_reg[2];
+BYTE DE_reg[2];
+BYTE HL_reg[2];
+WORD SP; //stack pointer
+WORD PC; //program counter
+
+//sub-registers
+
+BYTE *A = &AF_reg[0]; //pointer to sub-register (first byte)
+BYTE *B = &BC_reg[0];
+BYTE *C = &BC_reg[1];
+BYTE *D = &DE_reg[0];
+BYTE *E = &DE_reg[1];
+BYTE *H = &HL_reg[0];
+BYTE *L = &HL_reg[1];
+
+//flags
+//may not work, check bitwise arithemtic
+BYTE z = AF_reg[1] & 0x80; //zero flag, 7th bit
+BYTE n = AF_reg[1] & 0x40; //subtraction flag
+BYTE h = AF_reg[1] & 0x20; //half carry flag
+BYTE c = AF_reg[1] & 0x10; //carry flag
+
 int main() {
     std::cout << "Hello, !!!!!!!!" << std::endl;
     //nothing
