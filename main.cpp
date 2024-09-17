ef237c82812aae1e11d54024fa73c71d41b6c277
Serjongo <Serjongo@gmail.com>
2024-09-17 19:01:47 +0300

    Restructuring the project to be object-oriented.

--- main.cpp
+++ main.cpp
@@ -66,7 +66,8 @@ class gameboy
 
         //array of pointers to sub-registers. the way it works is by casting the pointers of unsigned shorts (WORDs) into pointers of bytes.
         // In order to point to the second byte of said short, we increment the pointer by 1(and since we're talking in BYTE resolution, this increments us by 8 bits)
-            Register* r8[8] = {BC_reg.hi,&BC_reg.lo, &DE_reg.hi,&DE_reg.lo,&HL_reg.hi,&HL_reg.lo,(BYTE*)&mem[HL_reg],&AF_reg.hi};
+            BYTE* r8[8] = {&BC_reg.hi,&BC_reg.lo, &DE_reg.hi,&DE_reg.lo,&HL_reg.hi,&HL_reg.lo,(BYTE*)&mem[HL_reg.lo],&AF_reg.hi}; //index 6 is PROBLEMATIC,
+            // (IT POINTS TO BYTE L, WE NEED TO CAST TO WORD* EVERY TIME WE POINT TO IT)
 
 
             //for redability
@@ -74,15 +75,6 @@ class gameboy
             //BYTE *OP_0 = &OPCODE;
             //BYTE *OP_1 = &OPCODE;
 
-            //sub-registers
-            //
-            //BYTE *A = &AF_reg[0]; //pointer to sub-register (first byte)
-            //BYTE *B = &BC_reg[0];
-            //BYTE *C = &BC_reg[1];
-            //BYTE *D = &DE_reg[0];
-            //BYTE *E = &DE_reg[1];
-            //BYTE *H = &HL_reg[0];
-            //BYTE *L = &HL_reg[1];
 
             //flags
             //may not work, check bitwise arithemtic
@@ -132,6 +124,74 @@ class gameboy
                 mem[0xFF4A] = 0x00 ;
                 mem[0xFF4B] = 0x00 ;
                 mem[0xFFFF] = 0x00 ;
+            }
+
+            void fetch()
+            {
+                OPCODE = mem[PC];
+                PC++;
+            }
+            void decode_execute()
+            {
+                switch(OPCODE) //MAY NEED TO FIX THE OPCODE SINCE ITS 2 BYTES, BUT MAYBE ITS OK SINCE IT SHOULD BE 0 anyway
+                {
+                    case(0x00): //NOP OPERATION
+                        break;
+
+                    case(0x01):case(0x11):case(0x21):case(0x31): //LD BC, d16
+                        //r16[4th&5th_bits] = memory[PC] which is 2 bytes
+                        //increment PC twice
+                        tmp = (OPCODE & 0x30)>>4;
+                        *r16[tmp].reg = mem[PC];
+                        PC++;
+                        PC++;
+                        break;
+
+
+                    case(0x02): case(0x12): //LD (BC) OR (DE), A
+                        tmp = (OPCODE & 0x30)>>4;
+                        mem[*r16[tmp]] = *r8[7];
+                        break;
+
+                        ///TO CHECK FIRST THING - IS A "TMP" still relevant in this opcode?! I DONT THINK SO, DELETING TMP FOR NOW
+                    case(0x22): case(0x32): //LD (HL), A
+                        //tmp = (OPCODE & 0x30)>>4;
+                        mem[*r16[2]] = *r8[7]; //IS IT POINTER OR NUMBER, RE-CHECK!
+                        r16[2]++; //increment the CONTENTS of HL, which is a pointer to
+
+                    case(0x03):case(0x13):case(0x23):case(0x33): //INC r16[reg]
+                        tmp = (OPCODE & 0x30)>>4; //relevant opcode bits in r16 are 4th & 5th
+                        r16[tmp]++;
+                        break;
+
+
+                    case(0x04):case(0x14):case(0x24):case(0x34): //INC r8[reg]
+                        tmp = (OPCODE & 0x38)>>3; //relevant opcode bits in r8 are 3rd, 4th & 5th
+                        (WORD*)r8[tmp]++; ///temporary solution of casting the byte as a word, so it increments in the right place
+                        break;
+
+                    case(0x05):case(0x15):case(0x25):case(0x35): //DEC r8[reg]
+                        tmp = (OPCODE & 0x38)>>3;
+                        (WORD*)r8[tmp]--; ///temporary solution of casting the byte as a word, so it increments in the right place
+                        break;
+
+                    default:
+                        break;
+
+
+
+
+                }
+            }
+
+            void main_loop()
+            {
+                while(true)
+                {
+                    fetch();
+                    decode_execute();
+                }
+
             }
 
             //public methods
@@ -241,61 +301,12 @@ int main() {
         //main gameboy loop
 
         //FETCH
-        OPCODE = mem[PC_reg];
-        PC_reg++;
+
 
         //DECODE & EXECUTE
 
         //FIRST BYTE - OPCODE SWITCH CASE
-        switch(OPCODE) //MAY NEED TO FIX THE OPCODE SINCE ITS 2 BYTES, BUT MAYBE ITS OK SINCE IT SHOULD BE 0 anyway
-        {
-            case(0x00): //NOP OPERATION
-                break;
 
-            case(0x01):case(0x11):case(0x21):case(0x31): //LD BC, d16
-                //r16[4th&5th_bits] = memory[PC] which is 2 bytes
-                //increment PC twice
-                tmp = (OPCODE & 0x30)>>4;
-                *r16[tmp] = mem[PC_reg];
-                PC_reg++;
-                PC_reg++;
-                break;
-
-
-            case(0x02): case(0x12): //LD (BC) OR (DE), A
-                tmp = (OPCODE & 0x30)>>4;
-                mem[*r16[tmp]] = *r8[7];
-                break;
-
-            ///TO CHECK FIRST THING - IS A "TMP" still relevant in this opcode?! I DONT THINK SO, DELETING TMP FOR NOW
-            case(0x22): case(0x32): //LD (HL), A
-                //tmp = (OPCODE & 0x30)>>4;
-                mem[*r16[2]] = *r8[7]; //IS IT POINTER OR NUMBER, RE-CHECK!
-                r16[2]++; //increment the CONTENTS of HL, which is a pointer to
-
-            case(0x03):case(0x13):case(0x23):case(0x33): //INC r16[reg]
-                tmp = (OPCODE & 0x30)>>4; //relevant opcode bits in r16 are 4th & 5th
-                r16[tmp]++;
-                break;
-
-
-            case(0x04):case(0x14):case(0x24):case(0x34): //INC r8[reg]
-                tmp = (OPCODE & 0x38)>>3; //relevant opcode bits in r8 are 3rd, 4th & 5th
-                (WORD*)r8[tmp]++; ///temporary solution of casting the byte as a word, so it increments in the right place
-                break;
-
-            case(0x05):case(0x15):case(0x25):case(0x35): //DEC r8[reg]
-                tmp = (OPCODE & 0x38)>>3;
-                (WORD*)r8[tmp]--; ///temporary solution of casting the byte as a word, so it increments in the right place
-                break;
-
-            default:
-                break;
-
-
-
-
-        }
 
         //nothing
     }
