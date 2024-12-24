af226467efd037949f5a3a94204dffe0c95dfeaa
Serjongo <Serjongo@gmail.com>
2024-12-24 19:00:19 +0200

    implementing ppu, working on the pixel fetcher

--- PPU.cpp
+++ PPU.cpp
@@ -10,8 +10,17 @@ PPU::PPU(BYTE* OAM_start,BYTE* VRAM_start, BYTE* MEM_start)
     MEM = MEM_start;
 }
 
-int* PPU::OAM_SCAN()
+void PPU::clean_visible_OAM_buff()
 {
+    for(int i = 0 ; i < 10;i++) //size of the array
+    {
+        visible_OAM_buffer[i] = nullptr;
+    }
+}
+
+void PPU::OAM_SCAN() //mode 2 of the ppu
+{
+    clean_visible_OAM_buff();
     // start of every scanline, find sprites to be rendered, add them to buffer given the following conditions per sprites:
     // xpos > 0, LY+16 > ypos, LY+16 < ypos+height (16 or 8), amount of spirtes < 10
     int sprites_in_buff = 0;
@@ -21,7 +30,7 @@ int* PPU::OAM_SCAN()
     {
         BYTE y_pos = OAM[i];
         BYTE x_pos = OAM[i+1];
-        BYTE tile_idx = OAM[i+2];
+        BYTE tile_num = OAM[i+2];
         BYTE attributes = OAM[i+3];
 
         if(x_pos > 0 && cur_row + 16 >= y_pos && cur_row + 16 < y_pos + tile_size && sprites_in_buff < 10)
@@ -30,12 +39,65 @@ int* PPU::OAM_SCAN()
             sprites_in_buff++;
         }
     };
+}
+
+void PPU::DRAW() //mode 3 of the ppu
+{
+    //calculate lcd_x_coord...
+    //add it to tile_dat_mem when calling pixel_fetcher
+    //pixel_fetcher(tile_dat_mem+lcd_x_coord,lcd_x_coord);
+}
+
+void PPU::pixel_fetcher(WORD tile_dat_mem,int lcd_x_coord)
+{
+    //placeholder
+    int tile_dat_pixel_index = 0;
+
+    WORD tilemap_mem_loc = 0x9800; //window location
+
+    //may add in the future a check for bit 5. if bit 5 is off, window is to be absolutely ignored regardless of other bits, and background will be drawn instead.
+    if(get_LCDC_bg_tile_map_select_status() || get_LCDC_window_tile_map_select())
+        tilemap_mem_loc = 0x9C00;
+
+    BYTE tile_num = MEM[tile_dat_mem];
+    //check if ppu is rendering background or window (check if one of the bits is on)
+
+
+    if (lcd_x_coord < WX_reg) //background rendering
+    {
+        MEM[SCX]/8 +
+    }
+    else if (lcd_x_coord >= WX_reg) //window rendering
+    {
+
+    }
+
+
+
+
+
+    //if 0, not window
 
 }
 
 BYTE PPU::get_LCDC_sprite_size_status() const
 {
     return (MEM[0xFF40] & (BYTE)(1 << 2)) == (BYTE)(1 << 2);
+};
+
+BYTE PPU::get_LCDC_window_display_enable_status() const
+{
+    return (MEM[0xFF40] & (BYTE)(1 << 5)) == (BYTE)(1 << 5);
+};
+
+BYTE PPU::get_LCDC_bg_tile_map_select_status() const
+{
+    return (MEM[0xFF40] & (BYTE)(1 << 3)) == (BYTE)(1 << 3);
+};
+
+BYTE PPU::get_LCDC_window_tile_map_select() const
+{
+    return (MEM[0xFF40] & (BYTE)(1 << 6)) == (BYTE)(1 << 6);
 };
 
 
--- PPU.h
+++ PPU.h
@@ -11,7 +11,12 @@ class PPU{
 public:
     const WORD VRAM_mem_start = 0x8000;
     const WORD VRAM_mem_end = 0x97ff;
+    const WORD WX_reg = 0xFF4A; //leftmost x_coord border of the window (continues till end of screen)
+    const WORD WY_reg = 0xFF4B; //top y_coord border of the window
     const int oam_size = 0xA0; //160 bytes
+    const WORD SCX = 0xFF42; //x-screen coordinate for rendering
+    const WORD SCY = 0xFF43; //y-scrren coordinate for rendering
+
 
 
     BYTE* VRAM; // from the vram start point
@@ -19,6 +24,7 @@ public:
     BYTE* MEM; //1 to 1 memory mapping from the start
 
     BYTE* visible_OAM_buffer[0x0A]; //pointers to 10 OAMs/sprites
+    void pixel_fetcher(WORD tile_num,int lcd_x_coord);
 
     PPU(BYTE* OAM_start,BYTE* VRAM_start,BYTE* MEM_start); //
 
@@ -41,10 +47,15 @@ public:
 
     //h-blank (mode 0) - after every line
 
-    int* OAM_SCAN();
+    void OAM_SCAN();
+    void clean_visible_OAM_buff();
+    void DRAW();
 
     //getters
     BYTE get_LCDC_sprite_size_status() const; // 1 means tall (8X16 pixels),0 = 8x8
+    BYTE get_LCDC_window_display_enable_status() const;
+    BYTE get_LCDC_bg_tile_map_select_status() const;
+    BYTE get_LCDC_window_tile_map_select() const;
     //setters
 
 
